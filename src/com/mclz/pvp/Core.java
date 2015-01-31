f5bc8b40f50f6ff4d252384599b10462283f3961
Robert <Robert@Roberts-Mac-Book-2.local>
2015-01-31 10:13:37 -0500

    Initial commit

---
+++ src/com/mclz/pvp/Core.java
@@ -0,0 +1,29 @@
+package com.mclz.pvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * ***********************************************************************
+ * Copyright PixeledCow (C) 2015. All Rights Reserved.
+ * Any code contained within this document, and any associated APIs with similar branding
+ * are the sole property of PixeledCow. Distribution, reproduction, taking snippets, or
+ * claiming any contents as your own will break the terms of the liscense, and void any
+ * agreements with you, the third party.
+ * Thanks.
+ * ************************************************************************
+ */
+
+public class Core extends JavaPlugin  {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+
+}
